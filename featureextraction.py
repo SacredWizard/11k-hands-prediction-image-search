7d42ffd237125b1d502559e4e68cc29702728648
frost <93@asu.edu>
2019-09-27 15:36:51 -0700

    Updating feature Extraction

--- featureextraction.py
+++ featureextraction.py
@@ -11,14 +11,16 @@ Authors:
 This is a module for performing feature extraction on images
 """
 
-from skimage.feature import hog, local_binary_pattern
-from skimage import data, exposure, io, color
-from skimage.transform import downscale_local_mean, rescale
 import os
-import numpy
-import matplotlib.pyplot as plt
-from pymongo import MongoClient
 import sys
+
+import cv2
+import matplotlib.pyplot as plt
+import numpy as np
+from pymongo import MongoClient
+from skimage import io, color
+from skimage.feature import hog, local_binary_pattern
+from skimage.transform import downscale_local_mean, rescale
 
 
 class ExtractFeatures:
@@ -86,18 +88,31 @@ class ExtractFeatures:
                 # appends the lbp vector for each window into a concatenated list
                 lbp_list.append(lbp.tolist())
 
-        lbp_list = numpy.asarray(lbp_list).ravel()
+        lbp_list = np.asarray(lbp_list).ravel()
         self.feature = str(lbp_list.tolist())
 
     def extract_sift(self):
-        pass
+        """
+        Calculate SIFT Features for an image
+        :param img: Image in BGR Format (Default format used to read Image in OpenCV)
+        :param image_id: Image ID (An Identifier for an Image)
+        :return:
+        """
+        image_id = self.image
+        img = cv2.imread(os.path.join(self.folder, self.image))
+        gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        sft = cv2.xfeatures2d.SIFT_create()
+        kp, des = sft.detectAndCompute(gray_img, None)
+        sift_features = [{"keyPoint": i, "x": kp[i].pt[0], "y": kp[i].pt[1], "angle": kp[i].angle,
+                          "size": kp[i].size, "descriptor": des[i].tolist()} for i in range(len(kp))]
+        self.feature = {"imageId": image_id, "kpCount": len(kp), "features": sift_features}
 
     def extract_hog(self, show=True):
         """Method for extracting histogram of oriented gradients"""
         image = io.imread(os.path.join(self.folder, self.image))
         down_image = downscale_local_mean(image, (10, 10, 1))
         rescale_image = rescale(image, 0.1, anti_aliasing=True)
-        down_image = down_image.astype(numpy.uint8)
+        down_image = down_image.astype(np.uint8)
 
         fd, hog_image = hog(rescale_image, orientations=9, pixels_per_cell=(8, 8),
                             cells_per_block=(2, 2), visualize=True, block_norm='L2', multichannel=True,
@@ -129,10 +144,70 @@ class ExtractFeatures:
             self.save_feature_mongo()
         print("Successfully Inserted Feature Vectors for {} images".format(len(image_list)))
 
-    def extract_cm(self):
-        """Method for extracting color moments"""
-        features = [self.image]
-        return features
+    def extract_cm(self, window_size=100):
+        """
+        Calculate Color Moments for an Image (Image is converted to yuv format)
+        Three color moments are computed namely, Mean, Standard Deviation, Skew
+        Image is divided into windows specified by a window size and then Color Moments are computed
+        Size of a block = window_size * window_size
+        :param img: Image in BGR Format (Default format used to read Image in OpenCV)
+        :param image_id: Image ID (An Identifier for an Image)
+        :param window_size: The Window size of the image used to split the image into blocks having a size of window
+        :return: Computed Color Moments
+        """
+        image_id = self.image
+        img = cv2.imread(os.path.join(self.folder, self.image))
+        img_yuv = self.bgr2yuv(img)
+        (rows, cols) = img.shape[:2]
+        data = {"imageId": image_id, "type": "CM"}
+        counter = 0
+        y, u, v = [], [], []
+
+        for i in range(0, int(rows / window_size)):
+            for j in range(0, int(cols / window_size)):
+                (mean, std_dev) = cv2.meanStdDev(
+                    img_yuv[i * window_size:(i + 1) * window_size, j * window_size:(j + 1) * window_size])
+                skw = self.skew(img_yuv[i * window_size:(i + 1) * window_size, j * window_size:(j + 1) * window_size])
+                counter += 1
+                y = y + [mean[0][0], std_dev[0][0], skw[0]]
+                u = u + [mean[1][0], std_dev[1][0], skw[1]]
+                v = v + [mean[2][0], std_dev[2][0], skw[2]]
+        data["y"] = y
+        data["u"] = u
+        data["v"] = v
+        self.feature = data
+
+    def skew(self, img):
+        """
+        Calculate Skew for an image
+        :param img: Image
+        :return: Skew for individual image channels
+        """
+        (rows, cols, colors) = img.shape
+        mean = [np.mean(img[:, :, i]) for i in range(colors)]
+        return \
+            [(np.cbrt(np.divide(np.sum(np.power(np.subtract(img[:, :, i], mean[i]), 3)), rows * cols))) for i in
+             range(colors)]
+
+    def bgr2yuv(self, img):
+        """
+        Convert a BGR Image to YUV Image
+        :param img: BGR Image
+        :return: YUV Image
+        """
+        return cv2.cvtColor(img, cv2.COLOR_BGR2YUV)
+
+    def skew(self, img):
+        """
+        Calculate Skew for an image
+        :param img: Image
+        :return: Skew for individual image channels
+        """
+        (rows, cols, colors) = img.shape
+        mean = [np.mean(img[:, :, i]) for i in range(colors)]
+        return \
+            [(np.cbrt(np.divide(np.sum(np.power(np.subtract(img[:, :, i], mean[i]), 3)), rows * cols))) for i in
+             range(colors)]
 
     def save_feature_mongo(self):
         """Method for saving extracted features to mongo"""
