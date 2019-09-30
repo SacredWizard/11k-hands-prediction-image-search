01d178565049298f43cb47c7f199e3edf0cd0b84
Shantanu Gupta <shantanu.smg@gmail.com>
2019-09-29 18:28:55 -0700

    code for pca

--- pca.py
+++ pca.py
@@ -15,45 +15,57 @@ from pymongo import MongoClient
 from skimage import io, color
 from skimage.feature import hog, local_binary_pattern
 from skimage.transform import downscale_local_mean, rescale
-
-
+from sklearn.decomposition import PCA
 
 mongo_client = MongoClient()
 db = mongo_client.features
 coll = db.hog
-arr = []
-for i,rec in enumerate(coll.find()):
-    arr.append(rec["featureVector"])
-print(arr)
+arr = [[1,2,3],[3,2,1],[2,2,2]]
+
+# for i,rec in enumerate(coll.find()):
+#     arr.append(rec["featureVector"])
+
+# print(arr)
 print(np.shape(arr))
 
+#####################
 meanmatrix = np.mean(arr, axis = 0)
-print(meanmatrix)
+# print(meanmatrix)
 meanNormalized = arr
 print(np.shape(meanmatrix))
 for i,row in enumerate(arr):
     meanNormalized[i] = np.array(arr[i] - meanmatrix)
 # arr = np.var(arr, axis = 1)
-print(meanNormalized)
+# print(meanNormalized)
 print(np.shape(meanNormalized))
 covMatrix = np.cov(arr, rowvar=False)
+
+print ("xyz")
 print(np.shape(covMatrix))
-print(covMatrix)
+# print(covMatrix)
 eigVal,eigVec = np.linalg.eig(covMatrix)
 
 print("Eigen values", np.shape(eigVal))
-print(eigVal)
+# print(eigVal)
 print("Eigen vectors", np.shape(eigVec))
-print(eigVec)
+# print(eigVec)
 
 sorted = eigVal.argsort()[::-1]
 eigVal,eigVec = eigVal[sorted], eigVec[:, sorted]
 
 print("after sort")
 print("Eigen values", np.shape(eigVal))
-print(eigVal)
+# print(eigVal)
 print("Eigen vectors", np.shape(eigVec))
-print(eigVec)
+# print(eigVec)
 prinComponents = np.dot(meanNormalized,eigVec)
-print(prinComponents[:][:5])
+# print(prinComponents[:][:5])
 print("Principal components", np.shape(prinComponents))
+#################
+
+# print(arr)
+# pca = PCA(n_components = 3,copy=False)
+# decompsedArr = pca.fit(arr)
+# print("decompsedArr",decompsedArr)
+# transformArr = decompsedArr.transform(arr)
+# print("transformArr",transformArr)
