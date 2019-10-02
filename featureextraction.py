952a2b3913c954df89512e862c38909efe0570db
Shantanu Gupta <shantanu.smg@gmail.com>
2019-10-01 23:25:55 -0700

    lbp with histogram

--- featureextraction.py
+++ featureextraction.py
@@ -77,16 +77,18 @@ class ExtractFeatures:
         # dimensions for 100x100 windows for the image
         window_rows = 100
         window_columns = 100
+        _bins= [0, 1, 2, 3, 4, 6, 7, 8, 12, 14, 15, 16, 24, 28, 30, 31, 32, 48, 56, 60, 62, 63, 64, 96, 112, 120, 124, 126, 127, 128, 129, 131, 135, 143, 159, 191, 192, 193, 195, 199, 207, 223, 224, 225, 227, 231, 239, 240, 241, 243, 247, 248, 249, 251, 252, 253, 254, 255, 256]
 
         lbp_list = []
-
         # splits the 1600x1200 image into 192 blocks of 100x100 pixels and calculates lbp vector for each block
         for row in range(0, image.shape[0], window_rows):
             for column in range(0, image.shape[1], window_columns):
                 window = image[row:row + window_rows, column:column + window_columns]
                 lbp = local_binary_pattern(window, num_points, radius, 'uniform')
+                _windowHistogram = np.histogram(lbp, bins=_bins)[0]
+
                 # appends the lbp vector for each window into a concatenated list
-                lbp_list.append(lbp.tolist())
+                lbp_list.append(_windowHistogram.tolist())
 
         lbp_list = np.asarray(lbp_list).ravel()
         self.feature = str(lbp_list.tolist())
