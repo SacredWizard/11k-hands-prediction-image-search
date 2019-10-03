a1281b861a2cb9656176e0a8f71a73ab3f02bb73
karishma-joseph <kjoseph7@asu.edu>
2019-10-02 20:47:40 -0700

    Added functionality to retrieve feature matrix from database

--- svd.py
+++ svd.py
@@ -10,9 +10,14 @@ from numpy import array
 from numpy import diag
 from numpy import zeros
 from numpy import argsort
-from scipy.linalg import svd
+import pymongo
+import numpy as np
 
+from scipy.linalg import svd
+from classes import global_connections, global_constants
 from classes.featureextraction import ExtractFeatures
+
+connection = global_connections.GlobalConnections()
 
 def implement_svd(feature_matrix, k, feature_descriptor):
 
@@ -33,7 +38,7 @@ def implement_svd(feature_matrix, k, feature_descriptor):
 def getDataLatentSemantics(U_matrix, k):
     term = [argsort(-U_matrix[:, i]) for i in range(k)]
     TW=[]
-    #for each latent semantic
+    #for each latent semantic (k)
     for i in range(k):
         kTW = []
         #for each data object
@@ -45,7 +50,7 @@ def getDataLatentSemantics(U_matrix, k):
 def getFeatureLatentSemantics(VT_matrix, k):
     term = [argsort(-VT_matrix[i, :]) for i in range(k)]
     TW=[]
-    #for each latent semantic
+    #for each latent semantic (k)
     for i in range(k):
         kTW = []
         #for each feature
@@ -64,24 +69,33 @@ def saveTW(U_TW, VT_TW, feature_descriptor):
     f.close()
 
 def main():
-    # define a matrix
-    feature_matrix = array([
-        [1,1,1,0,0],
-        [3,3,3,0,0],
-        [4,4,4,0,0],
-        [5,5,5,0,0],
-        [0,2,0,4,4],
-        [0,0,0,5,5],
-        [0,1,0,2,2]])
-    k = 6
+    # define a matrixet_object_featur
+    feature_descriptor = 'CM'
+    feature_matrix = get_object_feature_matrix(feature_descriptor)
+    k = 3
     if (k > feature_matrix.shape[1]):
         print("This k value is invalid. Please enter a different k value.")
     else:
-        implement_svd(feature_matrix, k, "hog")
+        implement_svd(feature_matrix, k, feature_descriptor)
         print("Done")
 
-def createFeatureMatrix(feature_descriptor):
-    array = []
+def get_object_feature_matrix(extractor):
+    try:
+        vector_list = []
+        cursor = connection.mongo_client.features[extractor.lower()].find({}, {'_id': 0})
+        if extractor == 'LBP':
+            for rec in cursor:
+                vector_list.append(rec['featureVector'].split(','))
+            return np.array(vector_list).astype(np.float)
+
+        for rec in cursor:
+            vector_list.append(rec['featureVector'])
+        return np.array(vector_list)
+
+    except pymongo.errors.ServerSelectionTimeoutError as e:
+        print("Timeout:\n{}".format(e))
+    except Exception as e:
+        print("Exception occurred:\n{}".format(e))
 
 
 if __name__ == "__main__":
