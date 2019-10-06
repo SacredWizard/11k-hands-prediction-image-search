e0652447e54835a6bf5d8a60ef01c02ef2d527e7
karishma-joseph <kjoseph7@asu.edu>
2019-10-05 18:15:14 -0700

    Fixed the variable and method names for good practices

--- utils/termweight.py
+++ utils/termweight.py
@@ -1,38 +1,38 @@
 from numpy import argsort
 
-def getDataLatentSemantics(data_M, k):
-    term = [argsort(-data_M[:, i]) for i in range(k)]
-    TW=[]
+def get_data_latent_semantics(data_m, k):
+    term = [argsort(-data_m[:, i]) for i in range(k)]
+    tw=[]
     #for each latent semantic (k)
     for i in range(k):
-        kTW = []
+        k_tw = []
         #for each data object
         for j in range(len(term[i])):
-            kTW.append([term[i][j],data_M[term[i][j],i]])
-        TW.append(kTW)
-    return TW
+            k_tw.append([term[i][j],data_m[term[i][j],i]])
+        tw.append(k_tw)
+    return tw
 
-def getFeatureLatentSemantics(feature_M, k):
-    term = [argsort(-feature_M[i, :]) for i in range(k)]
-    TW=[]
+def get_feature_latent_semantics(feature_m, k):
+    term = [argsort(-feature_m[i, :]) for i in range(k)]
+    tw=[]
     #for each latent semantic (k)
     for i in range(k):
-        kTW = []
+        k_tw = []
         #for each feature
         for j in range(len(term[i])):
-            kTW.append([term[i][j],feature_M[i,term[i][j]]])
-        TW.append(kTW)
-    return TW
+            k_tw.append([term[i][j],feature_m[i,term[i][j]]])
+        tw.append(k_tw)
+    return tw
 
     #prints Term Weight Pairs to console
-def printTW(data_TW, feature_TW, feature_descriptor):
+def print_tw(data_tw, feature_tw, feature_descriptor):
     print()
     print("-------------------------------------------------------------------------------------------------------------")
     print()
-    print("Data Latent semantics\n")
-    print(data_TW)
+    print("Data Latent Semantics\n")
+    print(data_tw)
     print()
     print("-------------------------------------------------------------------------------------------------------------")
     print()
-    print("Feature Latent semantics\n")
-    print(feature_TW)
+    print("Feature Latent Semantics\n")
+    print(feature_tw)
