0a4eea220556f8487f9aaefb06b076ff322202c9
Shantanu Gupta <shantanu.smg@gmail.com>
2019-10-14 18:24:31 -0700

    save to csv

--- utils/excelcsv.py
+++ utils/excelcsv.py
@@ -7,7 +7,9 @@ Author : Sumukh Ashwin Kamath
 import json
 
 import pandas
-
+import csv
+import numpy as np
+from os import path,makedirs
 from classes.globalconstants import GlobalConstants
 from classes.mongo import MongoWrapper
 
@@ -24,3 +26,27 @@ class CSVReader:
         data_json = json.loads(data.to_json(orient='records'))
         self.mongo_wrapper.drop_collection(self.constants.METADATA)  # Drop Metadata Collection
         self.mongo_wrapper.bulk_insert(self.constants.METADATA, data_json)  # Insert new Metadata
+
+    # method to format rows to output to csv
+    def prepare_rows(self, latent_semantics):
+        round_digits = 2
+        result = []
+        for i,ls in enumerate(latent_semantics):
+            term_weight = {}
+            for x in ls:
+                term_weight[x[0]] = round(x[1], round_digits)
+            result.append(("LS"+str(i+1)+", "+str(term_weight)[1:-1]).split(','))
+        return result
+
+    # method to save latent semantics to csv
+    def save_to_csv(self, data_latent_semantics, feature_latent_semantics):
+        current_path = path.dirname(path.dirname(path.realpath(__file__)))
+        _finalPath = path.join(current_path,"output")
+        if not path.exists(_finalPath):
+            makedirs((_finalPath))
+        with open(path.join(_finalPath, "latent_semantics.csv"), mode='w', newline='') as csv_file:
+            csv_writer = csv.writer(csv_file, delimiter=",")
+            csv_writer.writerow(["Data Latent Semantics"])
+            csv_writer.writerows(self.prepare_rows(data_latent_semantics))
+            csv_writer.writerow(["Feature Latent Semantics"])
+            csv_writer.writerows(self.prepare_rows(feature_latent_semantics))
