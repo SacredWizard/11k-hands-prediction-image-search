d22b2507fd470a74f58dc48db26bf4ca1d07d405
AshwinKamath42 <ashwin.sumukh@gmail.com>
2019-10-19 14:41:39 -0700

    Adding saving of output visualizer images to Output folder for task 2 and 4

--- utils/imageviewer.py
+++ utils/imageviewer.py
@@ -11,12 +11,15 @@ from skimage import io
 
 def show_images(query_image, image_list, title):
     """Visualizer for the images"""
-    f = plt.figure()
-    f.suptitle(title, fontsize=20)
+    f = plt.figure(figsize=(20, 12))
+    title_visualizer = ""
+    for i in title:
+        title_visualizer += i + ":" + str(title[i]) + "  "
+
+    f.suptitle(title_visualizer, fontsize=18)
 
     no_images_per_row = 5
     no_of_lines = int(len(image_list) / no_images_per_row + 1)
-    print(no_of_lines)
 
     f.add_subplot(no_of_lines, 5, 1)
     plt.imshow(io.imread(query_image))
@@ -31,4 +34,11 @@ def show_images(query_image, image_list, title):
             "{}\nScore: {}%".format(r['imageId'], round(r['score'], 3)))
         plt.axis('off')
         count = count + 1
+
+    fig = plt.gcf()
+    fig.set_size_inches((20, 12), forward=True)
+    filename = "output/{}".format("_".join([str(i) for i in title.values()]) + "_" + query_image.split("/")[-1].
+                                  strip(".jpg"))
+    fig.savefig(filename, dpi=500)
     plt.show()
+
