ff64db7ffc654c5d2eddd8c2e0f2d036ff2ac4cf
AshwinKamath42 <ashwin.sumukh@gmail.com>
2019-10-18 17:44:49 -0700

    Adding check for negative values in LDA and NMF

--- phase2_cli.py
+++ phase2_cli.py
@@ -1,3 +1,9 @@
+"""
+Measuring Similarity of Hand Images Using Color Moments and Histogram of Oriented Gradients
+This is the CLI for the Second Phase of the Project
+Author : Sumukh Ashwin Kamath
+(ASU ID - 1217728013 email - skamath6@asu.edu
+"""
 from utils.inputhelper import get_task_number
 import importlib
 
@@ -5,7 +11,7 @@ import importlib
 def main():
     """Main function for the script"""
     number_of_tasks = 8
-    choice = get_task_number(8)
+    choice = get_task_number(number_of_tasks)
     module_name = "task{}".format(choice)
     module = importlib.import_module(module_name)
     module.main()
