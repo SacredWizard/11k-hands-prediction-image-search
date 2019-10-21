5e8750e9028f45ea34c0229a5eac4434e5980255
karishma-joseph <kjoseph7@asu.edu>
2019-10-20 17:21:39 -0700

    Commented extra credit part for task 3

--- task3.py
+++ task3.py
@@ -13,6 +13,7 @@ This is the CLI for task 3 of Phase 2 of the project
 from classes.dimensionreduction import DimensionReduction
 from classes.globalconstants import GlobalConstants
 from utils.excelcsv import CSVReader
+from utils.imageviewer import show_feature_ls, show_data_ls
 from utils.inputhelper import get_input_k, get_input_dimensionality_reduction_model, \
     get_input_feature_extractor_model, get_input_image_label
 from utils.model import Model
@@ -53,21 +54,22 @@ def main():
     model_interact.save_model(model=model, filename=filename)
 
     # Printing the term weight pairs
-    print_tw(obj_lat, feat_lat)
+    data_tw = print_tw(obj_lat, feat_lat)
 
     # save term weight pairs to csv
     filename = "task3_{}_{}_{}_{}".format(feature_extraction_model, dimension_reduction_model, label, k_value)
     csv_reader.save_to_csv(obj_lat, feat_lat, filename)
     print("Please check the CSV file: output/{}.csv".format(filename))
 
-    data = dim_reduction.get_object_feature_matrix()
+    # data = dim_reduction.get_object_feature_matrix()
     
-    title = {
-        "Feature Extraction": feature_extraction_model,
-        "Dimensionality Reduction": dimension_reduction_model,
-        "k": k_value,
-    }
-    show_feature_ls(data, feat_lat, title)
+    # title = {
+    #     "Feature Extraction": feature_extraction_model,
+    #     "Dimensionality Reduction": dimension_reduction_model,
+    #     "k": k_value,
+    # }
+    # show_data_ls(data, data_tw, title)
+    # show_feature_ls(data, feat_lat, title)
 
 
 if __name__ == "__main__":
