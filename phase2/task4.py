f169e6c0ee13537b288fa878080377c4720f0356
Rakesh R <rakesh2693@gmail.com>
2019-11-17 23:39:12 -0700

    Moving Phase 2, and Adding files for phase3

---
+++ phase2/task4.py
@@ -0,0 +1,87 @@
+"""
+Multimedia Web Databases - Fall 2019: Project Group 17
+Authors:
+1. Sumukh Ashwin Kamath
+2. Rakesh Ramesh
+3. Baani Khurana
+4. Karishma Joseph
+5. Shantanu Gupta
+6. Kanishk Bashyam
+
+This is the CLI for task 4 of Phase 2 of the project
+"""
+import os
+import sys
+
+from classes.dimensionreduction import DimensionReduction
+from classes.globalconstants import GlobalConstants
+from utils.imageviewer import show_images
+from utils.inputhelper import get_input_feature_extractor_model, get_input_dimensionality_reduction_model, \
+    get_input_folder, get_input_image, get_input_image_label, get_input_k, get_input_m
+from utils.model import Model
+
+model_interact = Model()
+global_constants = GlobalConstants()
+
+
+def main():
+    """Main function for the task 4"""
+    feature_extraction_model = get_input_feature_extractor_model()
+    dimension_reduction_model = get_input_dimensionality_reduction_model()
+    k_value = get_input_k()
+    label = get_input_image_label()
+    folder = get_input_folder()
+    image_name = get_input_image(folder)
+    m_value = get_input_m()
+
+    if dimension_reduction_model != "NMF":
+        dist_func = "euclidean"
+    elif feature_extraction_model in ["CM", "LBP"]:
+        dist_func = "nvsc1"
+    else:
+        dist_func = "euclidean"
+        # dist_func = "cosine"
+        # dist_func = "chebyshev"
+        # dist_func = "manhattan"
+        # dist_func = "chi_square"
+        # dist_func = "euclidean"
+
+    print(global_constants.LINE_SEPARATOR)
+    print("User Inputs summary")
+    print(global_constants.LINE_SEPARATOR)
+    print("Feature Extraction Model: {}\nDimensionality Reduction Model: {}\nk-value: {}\nLabel: {}\nFolder: {}\n"
+          "Image: {}\nm-value: {}".format(feature_extraction_model, dimension_reduction_model, k_value, label, folder,
+                                          image_name, m_value))
+    print(global_constants.LINE_SEPARATOR)
+
+    # Saves the returned model
+    filename = "{0}_{1}_{2}_{3}".format(feature_extraction_model, dimension_reduction_model, label.replace(" ", ''),
+                                        str(k_value))
+    model = model_interact.load_model(filename=filename)
+    if not model:
+        print("Please run Task 3 for {}, {}, {} and {}".format(feature_extraction_model, dimension_reduction_model,
+                                                               label, k_value))
+        sys.exit(1)
+
+    # Compute the reduced dimensions for the new query image and find m similar images
+    dim_reduction = DimensionReduction(feature_extraction_model, dimension_reduction_model, k_value, label)
+    result = dim_reduction.find_m_similar_images(model, m_value, folder, image_name, dist_func)
+    print(global_constants.LINE_SEPARATOR)
+    print("Similar Images")
+    print(global_constants.LINE_SEPARATOR)
+    for rec in result:
+        print(rec)
+    print(global_constants.LINE_SEPARATOR)
+
+    title = {
+        "Feature Extraction": feature_extraction_model,
+        "Dimension Reduction": dimension_reduction_model,
+        "k": k_value,
+        "Label": label,
+        "Distance": dist_func
+             }
+    show_images(os.path.abspath(os.path.join(folder, image_name)), result, title)
+
+
+if __name__ == "__main__":
+    main()
---
+++ phase2/task5.py
@@ -0,0 +1,120 @@
+"""
+Multimedia Web Databases - Fall 2019: Project Group 17
+Authors:
+1. Sumukh Ashwin Kamath
+2. Rakesh Ramesh
+3. Baani Khurana
+4. Karishma Joseph
+5. Shantanu Gupta
+6. Kanishk Bashyam
+This is the CLI for task 5 of Phase 2 of the project
+"""
+from classes.dimensionreduction import DimensionReduction
+from classes.globalconstants import GlobalConstants
+from utils.inputhelper import get_input_image_label, get_input_folder, get_input_image, \
+    get_input_dimensionality_reduction_model, get_input_feature_extractor_model, get_input_k
+from utils.model import Model
+
+model_interact = Model()
+global_constants = GlobalConstants()
+
+
+def run_task3(feature_extraction_model, dimension_reduction_model, label, k_value):
+    """Main function for the Task3"""
+    # Performs the dimensionality reduction
+    dim_reduction = DimensionReduction(feature_extraction_model, dimension_reduction_model, k_value, label)
+    obj_feature = dim_reduction.get_object_feature_matrix()
+    obj_lat, feat_lat, model = dim_reduction.execute()
+
+    # Saves the returned model
+    filename = "{0}_{1}_{2}_{3}".format(feature_extraction_model, dimension_reduction_model, label.replace(" ", ''),
+                                        str(k_value))
+    model_interact.save_model(model=model, filename=filename)
+
+
+def run_task4(feature_extraction_model, dimension_reduction_model, folder, image_name, dist_func, label, k_value,
+              m_value):
+    """Main function for the Task4"""
+
+    # Saves the returned model
+    filename = "{0}_{1}_{2}_{3}".format(feature_extraction_model, dimension_reduction_model, label.replace(" ", ''),
+                                        str(k_value))
+    model = model_interact.load_model(filename=filename)
+
+    # Compute the reduced dimensions for the new query image and find m similar images
+    dim_reduction = DimensionReduction(feature_extraction_model, dimension_reduction_model, k_value, label)
+    obj_feature = dim_reduction.get_object_feature_matrix()
+    result = dim_reduction.find_m_similar_images(model, m_value, folder, image_name, dist_func)
+    return result
+
+
+def get_class_labels(label):
+    class1_labels = ["left-hand", "dorsal", "with accessories", "male"]
+    class2_labels = ["right-hand", "palmar", "without accessories", "female"]
+    if label in class1_labels:
+        label_type = 1
+        index = class1_labels.index(label)
+    else:
+        label_type = 2
+        index = class2_labels.index(label)
+    if label_type == 1:
+        class1_label = label
+        class2_label = class2_labels[index]
+    else:
+        class2_label = label
+        class1_label = class1_labels[index]
+    
+    return class1_label, class2_label
+
+
+def main():
+    feature_extraction_model = get_input_feature_extractor_model()
+    dimension_reduction_model = get_input_dimensionality_reduction_model()
+    label = get_input_image_label()
+    k_value = get_input_k()
+    query_folder = get_input_folder()
+    image_name = get_input_image(query_folder)
+    m_value = 1
+
+    print(global_constants.LINE_SEPARATOR)
+    print("User Inputs summary")
+    print(global_constants.LINE_SEPARATOR)
+    print("Feature Extraction Model: {}\nDimensionality Reduction Model: {}\nLabel: {}\nk-value: {}\nQuery Folder: {}"
+          "\nImage: {}".format(feature_extraction_model, dimension_reduction_model, label, k_value, query_folder,
+                               image_name, m_value))
+    print(global_constants.LINE_SEPARATOR)
+
+    if dimension_reduction_model != "NMF":
+        dist_func = "euclidean"
+    elif feature_extraction_model in ["CM", "LBP"]:
+        dist_func = "nvsc1"
+    else:
+        dist_func = "euclidean"
+        # dist_func = "cosine"
+        # dist_func = "chebyshev"
+        # dist_func = "manhattan"
+        # dist_func = "chi_square"
+        # dist_func = "euclidean"
+
+    class1_label, class2_label = get_class_labels(label)
+
+    run_task3(feature_extraction_model, dimension_reduction_model, class1_label, k_value)
+    result1 = run_task4(feature_extraction_model, dimension_reduction_model, query_folder, image_name, dist_func,
+                        class1_label, k_value, m_value)
+    class1_score = result1[0]['score']
+    # print(class1_score)
+
+    run_task3(feature_extraction_model, dimension_reduction_model, class2_label, k_value)
+    result2 = run_task4(feature_extraction_model, dimension_reduction_model, query_folder, image_name, dist_func,
+                        class2_label, k_value, m_value)
+    class2_score = result2[0]['score']
+    # print(class2_score)
+
+    final_label = class1_label if class1_score > class2_score else class2_label
+    print(global_constants.LINE_SEPARATOR)
+    print("{} !!!".format(final_label.upper()))
+    print(global_constants.LINE_SEPARATOR)
+
+
+if __name__ == "__main__":
+    main()
