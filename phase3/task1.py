702bed77dbdb5ddc7e74c710e604072f9b83ecb3
Shantanu Gupta <shantanu.smg@gmail.com>
2019-11-20 13:47:21 -0700

    p3 task1 not final

--- phase3/task1.py
+++ phase3/task1.py
@@ -1,3 +1,6 @@
+import os
+import sys
+sys.path.append(os.path.split(sys.path[0])[0])
 from classes.dimensionreduction import DimensionReduction
 from utils.model import Model
 from utils.excelcsv import CSVReader
@@ -8,7 +11,6 @@ import pandas as pd
 import numpy as np
 import scipy.stats as stats
 import operator
-import os
 import utils.imageviewer as imgvwr
 import phase2.task1 as p1task1
 import phase2.task5 as p2task5
@@ -30,12 +32,45 @@ def compute_latent_semantic(label, k_value):
     pass
 
 def main():
-    k_value = 40
-    
+
+
+    """
+            folder = get_input_folder()
+            model = get_input_feature_extractor_model()
+
+            feature_extractor = ExtractFeatures(folder, model)
+            feature_extractor.execute()
+
+
+    """
+    k_value = 10
+    # query_folder = get_input_folder()
+    training_set = 'C:\mwdb\commoncode\CSE515\Dataset3\Labelled\Set1'
+    test_set = 'C:\mwdb\commoncode\CSE515\Dataset3\\Unlabelled\Set 1'
+
     dim_reduction = DimensionReduction(feature_extraction_model, dimension_reduction_model, k_value)
+
+    dim_reduction.execute()
     p2task5.run_task3(feature_extraction_model, dimension_reduction_model, "dorsal", k_value)
 
-    pass
+    result_dorsal = []
+    for root, dirs, images in os.walk(test_set):
+        for image_name in images:
+            result_dorsal.append((p2task5.run_task4(feature_extraction_model, dimension_reduction_model, 
+                            training_set, image_name, dist_func, "dorsal", k_value, m_value=1))[0]['score'])
+    p2task5.run_task3(feature_extraction_model, dimension_reduction_model, "palmar", k_value)
+    result_palmar = []
+    for root, dirs, images in os.walk(test_set):
+        for image_name in images:
+            result_palmar.append((p2task5.run_task4(feature_extraction_model, dimension_reduction_model, 
+                            training_set, image_name, dist_func, "palmar", k_value, m_value=1))[0]['score'])
+
+    for root, dirs, images in os.walk(test_set):
+        for i, image_name in enumerate(images):
+            if result_dorsal[i] > result_palmar[i]:
+                print(image_name, ' : dorsal')
+            else:
+                print(image_name, ' : palmar')
 
 if __name__ == "__main__":
-    pass
+    main()
