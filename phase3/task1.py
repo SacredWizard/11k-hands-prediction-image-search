c57fdae02307b45241c6a0942d766c3d0c08ffd3
Shantanu Gupta <shantanu.smg@gmail.com>
2019-11-24 16:28:40 -0700

    p3 task1 compute latent semantics

--- phase3/task1.py
+++ phase3/task1.py
@@ -12,58 +12,74 @@ import numpy as np
 import scipy.stats as stats
 import operator
 import utils.imageviewer as imgvwr
+from utils.excelcsv import CSVReader
 import phase2.task1 as p1task1
 import phase2.task5 as p2task5
 import phase2.task6 as p2task6
 import time
-import random
 import warnings
+from sklearn.linear_model import LogisticRegression
+
 warnings.filterwarnings("ignore")
 model_interact = Model()
 global_constants = GlobalConstants()
 mongo_wrapper = MongoWrapper(global_constants.Mongo().DB_NAME)
+csv_reader = CSVReader()
 
-feature_extraction_model = "HOG"
-dimension_reduction_model = "NMF"
-dist_func = "euclidean"
+def compute_latent_semantic_for_label(feature_extraction_model, dimension_reduction_model, label, k_value):
 
-def compute_latent_semantic(label, k_value):
-    
-    pass
+    # p2task5.run_task3(feature_extraction_model, dimension_reduction_model, label, k_value)
+
+    dim_reduction = DimensionReduction(feature_extraction_model, dimension_reduction_model, k_value, label)
+    obj_lat, feat_lat, model = dim_reduction.execute()
+    # Saves the returned model
+    filename = "{0}_{1}_{2}_{3}".format(feature_extraction_model, dimension_reduction_model, label,
+                                        str(k_value))
+    model_interact.save_model(model=model, filename=filename)
+    return obj_lat,feat_lat, model
+
 
 def main():
-
-
     """
-            folder = get_input_folder()
-            model = get_input_feature_extractor_model()
+    folder = get_input_folder()
+    model = get_input_feature_extractor_model()
 
-            feature_extractor = ExtractFeatures(folder, model)
-            feature_extractor.execute()
-
-
+    feature_extractor = ExtractFeatures(folder, model)
+    feature_extractor.execute()
     """
-    k_value = 10
+    feature_extraction_model = "HOG"
+    dimension_reduction_model = "NMF"
+    dist_func = "euclidean"
+    k_value = 12
     # query_folder = get_input_folder()
     training_set = 'C:\mwdb\commoncode\CSE515\Dataset3\Labelled\Set1'
     test_set = 'C:\mwdb\commoncode\CSE515\Dataset3\\Unlabelled\Set 1'
+    label = "dorsal"
+    obj_lat_d,feat_lat_d, model_d = compute_latent_semantic_for_label(feature_extraction_model, dimension_reduction_model, label , k_value)
+    filename = "p3task1_{0}_{1}_{2}_{3}".format(feature_extraction_model, dimension_reduction_model, label, str(k_value))
+    csv_reader.save_to_csv(obj_lat_d, feat_lat_d, filename)
 
-    dim_reduction = DimensionReduction(feature_extraction_model, dimension_reduction_model, k_value)
+    # label = "palmar"
+    # obj_lat_p,feat_lat_p, model_p = compute_latent_semantic_for_label(feature_extraction_model, dimension_reduction_model, label, k_value)
+    # filename = "p3task1_{0}_{1}_{2}_{3}".format(feature_extraction_model, dimension_reduction_model, label, str(k_value))
+    # csv_reader.save_to_csv(obj_lat_p, feat_lat_p, filename)
 
-    dim_reduction.execute()
-    p2task5.run_task3(feature_extraction_model, dimension_reduction_model, "dorsal", k_value)
+    # x_train, x_test, y_train, y_test = feat_lat_d, test_set, feat_lat_p, test_set
+    # lr = LogisticRegression()
+    # lr.fit(x_train, y_train)
+    # x_pred = lr.predict(x_test)
+    # y_pred = lr.predict(y_test)
 
     result_dorsal = []
     for root, dirs, images in os.walk(test_set):
         for image_name in images:
             result_dorsal.append((p2task5.run_task4(feature_extraction_model, dimension_reduction_model, 
-                            training_set, image_name, dist_func, "dorsal", k_value, m_value=1))[0]['score'])
-    p2task5.run_task3(feature_extraction_model, dimension_reduction_model, "palmar", k_value)
+                            test_set, image_name, dist_func, "dorsal", k_value, m_value=1))[0]['score'])
     result_palmar = []
     for root, dirs, images in os.walk(test_set):
         for image_name in images:
             result_palmar.append((p2task5.run_task4(feature_extraction_model, dimension_reduction_model, 
-                            training_set, image_name, dist_func, "palmar", k_value, m_value=1))[0]['score'])
+                            test_set, image_name, dist_func, "palmar", k_value, m_value=1))[0]['score'])
 
     for root, dirs, images in os.walk(test_set):
         for i, image_name in enumerate(images):
@@ -72,5 +88,8 @@ def main():
             else:
                 print(image_name, ' : palmar')
 
+
 if __name__ == "__main__":
     main()
+
+    
