2896a23777deb8bbebe89810f0940f797ef66cc5
Baani Khurana <bkhurana@asu.edu>
2019-11-25 21:18:20 -0700

    Changed to SVM gaussian kernel

--- task4_svm.py
+++ task4_svm.py
@@ -8,10 +8,9 @@ from phase3.task1 import compute_latent_semantic_for_label, reduced_dimensions_f
 from classes.dimensionreduction import DimensionReduction
 from utils.model import Model
 from utils.excelcsv import CSVReader
-import numpy as np
 from numpy import linalg
-import cvxopt
-import cvxopt.solvers
+import cvxopt as optimizer
+import cvxopt.solvers as solver
 import random
 
 csv_reader = CSVReader()
@@ -25,67 +24,75 @@ def polynomial_kernel(x, y, p=3):
 def gaussian_kernel(x, y, sigma=5.0):
     return np.exp(-linalg.norm(x-y)**2 / (2 * (sigma ** 2)))
 
-class SVM(object):
+class SupportVectorMachine(object):
 
+    # initializing values
     def __init__(self, kernel=linear_kernel, C=None):
         self.kernel = kernel
         self.C = C
         if self.C is not None: self.C = float(self.C)
 
     def fit(self, X, y):
-        n_samples, n_features = X.shape
+        number_samples, number_features = X.shape
 
         # Gram matrix
         # initializing matrix of zeros and size of training data
-        K = np.zeros((n_samples, n_samples))
+        K = np.zeros((number_samples, number_samples))
 
         # getting polynomial kernel for each sample and storing in K
-        for i in range(n_samples):
-            for j in range(n_samples):
+        for i in range(number_samples):
+            for j in range(number_samples):
                 K[i,j] = self.kernel(X[i], X[j])
 
-        P = cvxopt.matrix(np.outer(y,y) * K)
-        q = cvxopt.matrix(np.ones(n_samples) * -1)
-        A = cvxopt.matrix(y, (1,n_samples), 'd')
-        b = cvxopt.matrix(0.0)
+        # G and A are sparse matrices
+        # P is a square dense or sparse real matrix, which represents a positive semidefinite symmetric matrix
+        # q is a real single-column dense matrix
+        # h and b are real-single column dense matrices
+        # G and A are real dense or sparse matrices
+
+        P = optimizer.matrix(np.outer(y,y) * K)
+        q = optimizer.matrix(np.ones(number_samples) * -1)
+        A = optimizer.matrix(y, (1,number_samples), 'd')
+        b = optimizer.matrix(0.0)
 
         if self.C is None:
-            G = cvxopt.matrix(np.diag(np.ones(n_samples) * -1))
-            h = cvxopt.matrix(np.zeros(n_samples))
+            G = optimizer.matrix(np.diag(np.ones(number_samples) * -1))
+            h = optimizer.matrix(np.zeros(number_samples))
         else:
-            tmp1 = np.diag(np.ones(n_samples) * -1)
-            tmp2 = np.identity(n_samples)
-            G = cvxopt.matrix(np.vstack((tmp1, tmp2)))
-            tmp1 = np.zeros(n_samples)
-            tmp2 = np.ones(n_samples) * self.C
-            h = cvxopt.matrix(np.hstack((tmp1, tmp2)))
+            tmp1 = np.diag(np.ones(number_samples) * -1)
+            tmp2 = np.identity(number_samples)
+            G = optimizer.matrix(np.vstack((tmp1, tmp2)))
+            tmp1 = np.zeros(number_samples)
+            tmp2 = np.ones(number_samples) * self.C
+            h = optimizer.matrix(np.hstack((tmp1, tmp2)))
 
-        # solve QP problem
-        solution = cvxopt.solvers.qp(P, q, G, h, A, b)
+        # solves quadratic programming problem
+        solution = solver.qp(P, q, G, h, A, b)
 
-        # Lagrange multipliers
+        # calculates Lagrange multipliers
         a = np.ravel(solution['x'])
 
-        # Support vectors have non zero lagrange multipliers
-        sv = a > 1e-5
-        ind = np.arange(len(a))[sv]
-        self.a = a[sv]
-        self.sv = X[sv]
-        self.sv_y = y[sv]
-        print("%d support vectors out of %d points" % (len(self.a), n_samples))
+        # support vectors have non zero lagrange multipliers
+        support_vectors = a > 1e-5
+        ind = np.arange(len(a))[support_vectors]
+        self.a = a[support_vectors]
+        self.support_vectors = X[support_vectors]
+        self.support_vectors_y = y[support_vectors]
+        print("---------------------------")
+        print("Support Vectors: " + str(len(self.a)))
 
-        # Intercept
+        # calculates b intercept
         self.b = 0
         for n in range(len(self.a)):
-            self.b += self.sv_y[n]
-            self.b -= np.sum(self.a * self.sv_y * K[ind[n],sv])
+            self.b += self.support_vectors_y[n]
+            self.b -= np.sum(self.a * self.support_vectors_y * K[ind[n],support_vectors])
         self.b /= len(self.a)
 
-        # Weight vector
+        # calculates the weights vector
         if self.kernel == linear_kernel:
-            self.w = np.zeros(n_features)
+            self.w = np.zeros(number_features)
             for n in range(len(self.a)):
-                self.w += self.a[n] * self.sv_y[n] * self.sv[n]
+                self.w += self.a[n] * self.support_vectors_y[n] * self.support_vectors[n]
         else:
             self.w = None
 
@@ -96,8 +103,8 @@ class SVM(object):
             y_predict = np.zeros(len(X))
             for i in range(len(X)):
                 s = 0
-                for a, sv_y, sv in zip(self.a, self.sv_y, self.sv):
-                    s += a * sv_y * self.kernel(X[i], sv)
+                for a, support_vectors_y, support_vectors in zip(self.a, self.support_vectors_y, self.support_vectors):
+                    s += a * support_vectors_y * self.kernel(X[i], support_vectors)
                 y_predict[i] = s
             return y_predict + self.b
 
@@ -109,7 +116,7 @@ if __name__ == "__main__":
     fea_ext_mod = "HOG"
     dim_red_mod = "PCA"
     dist_func = "euclidean"
-    k_value = 20
+    k_value = 30
     training_set = 'C:\\Users\\baani\OneDrive\Documents\Arizona State University\Fall 2019\CSE 515\Project\PhaseIII\CSE515\Dataset3\Labelled\Set1'
     test_set = 'C:\\Users\\baani\OneDrive\Documents\Arizona State University\Fall 2019\CSE 515\Project\PhaseIII\CSE515\Dataset3\\Unlabelled\Set 1'
     label = "dorsal"
@@ -150,19 +157,26 @@ if __name__ == "__main__":
     x_train = x_train[indices]
     y_train = y_train[indices]
 
-    print(y_train)
     x_test = np.array(x_test)
 
-    clf = SVM(polynomial_kernel, C=500)
+    # creates the SVM classifier
+    clf = SupportVectorMachine(gaussian_kernel, C=500)
     clf.fit(x_train, y_train)
     predictions = clf.predict(x_test)
 
-    print("Predictions:")
-    print(predictions)
-
     # transforms the testing labels into 1 for "dorsal", -1 for "palmar" data points 
     y_test = list(map(lambda x:1 if x=="dorsal" else -1,y_test))
-    print("Expected Output:")
-    print(y_test)
+
+    # calculates and prints the results onto the console
     correct = np.sum(predictions == y_test)
-    print("%d out of %d predictions correct" % (correct, len(predictions)))
+    print("---------------------------")
+    accuracy = (correct / len(predictions)) * 100
+    print("Accuracy: " + str(accuracy) + "%")
+    unlabelled_images = red_dim_unlabelled_images['imageId']
+    predicted_labels = list(map(lambda x:"dorsal" if x==1 else "palmar",predictions))
+    actual_labels = list(map(lambda x:"dorsal" if x==1 else "palmar",y_test))
+    print("---------------------------")
+    print("Results:")
+    print("Image ID, Prediction, Actual")
+    for image_id, p, a in zip(unlabelled_images, predicted_labels, actual_labels):
+        print("(" + image_id + ", " + p + ", " + a + ")")
