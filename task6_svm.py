07ccd2e82e388ccc52b35bf70b39aebe4c92a857
Shantanu Gupta <shantanu.smg@gmail.com>
2019-12-03 05:03:39 -0700

    task6 svm functional

--- task6_svm.py
+++ task6_svm.py
@@ -1,8 +1,8 @@
-import pdb
 import os
 import sys
 sys.path.append(os.path.split(sys.path[0])[0])
 from classes.dimensionreduction import DimensionReduction
+from classes.LSH import LSH
 from utils.model import Model
 import utils.relevancefeedback as relevancefeedback
 import numpy as np
@@ -12,6 +12,7 @@ import utils.imageviewer as imgvwr
 import phase2.task1 as p1task1
 import phase2.task1 as p2task1
 import phase3.task1 as p3task1
+import task5 as p3task5
 import time
 import warnings
 from task4_svm import SupportVectorMachine,gaussian_kernel
@@ -19,79 +20,71 @@ warnings.filterwarnings("ignore")
 
 from flask import Flask, request, render_template, send_from_directory,url_for
 app = Flask(__name__)
-
 model_interact = Model()
-
-"""
-Dummy. TO be replaced by actual LSH method
-"""
-def search_LSH(query_image):
-    fea_ext_mod = "HOG"
-    dim_red_mod = "SVD"
-    dist_func = "euclidean"
-    k_value = 30
-    t_value = 20
-    training_set = os.path.abspath('Dataset3\Labelled\Set2')
-    test_set = os.path.abspath('Dataset3\\Unlabelled\Set 1')
-    dim_reduction = DimensionReduction(fea_ext_mod, dim_red_mod, k_value)
-
-    filename = "{0}_{1}_{2}_{3}".format(fea_ext_mod, dim_red_mod, str(k_value), os.path.basename(training_set))
-    model = model_interact.load_model(filename=filename)
-    if model is None:
-        p2task1.save_model(dim_reduction, fea_ext_mod, dim_red_mod, str(k_value),folder=os.path.basename(training_set))
-        model = model_interact.load_model(filename=filename)
-    results = dim_reduction.find_m_similar_images(model, t_value, training_set, query_image, dist_func)
-    return results
-
-"""
-Method to incorporate relevance feedback
-"""
-def rerank_results(feedback):
-    # pdb.set_trace()
-    global feedback_imgs_g,feedback_vals_g,similar_images_g,similar_image_vectors_g
-
-    feedback = {'Hand_0000072.jpg': '1', 'Hand_0000073.jpg': '-1', 'Hand_0000074.jpg': '1', 'Hand_0000112.jpg': '-1'}
-    # Add SVM based relevance feedback function
-# """    clf = SupportVectorMachine(gaussian_kernel, C=500)
-#     feedback_imgs = list(feedback.keys())
-#     feedback_vals = list(feedback.values())
-# create commonf fn
-    # get_training_set()
-    # clf.fit(x_train, y_train)
-#     x_test = similar_images_g
-#     image_dist_SV = clf.project(x_test)
-#     rel_similar_images = sorted(image_dist_SV,reversed=True)
-#     """
-    rel_similar_images = ['Hand_0000072.jpg', 'Hand_0000073.jpg', 'Hand_0000074.jpg', 'Hand_0000112.jpg']
-    return rel_similar_images
-
-def get_training_set():
-    x_train=[]
-    y_train=[]
-    for i in range(similar_images_g):
-        for j in range(feedback_imgs_g):
-            if similar_images_g[i] == feedback_imgs_g[j]:
-                x_train.append(similar_image_vectors_g[i])
-                y_train.append(feedback_vals_g[j])
-    return x_train,y_train
-
+#global vars to retain feedback and initial set of images returned from LSH
 similar_images_g = []
 similar_image_vectors_g = []
 feedback_imgs_g = []
 feedback_vals_g = []
 
+def get_LSH_results(query_image):
+    lsh = LSH()
+    global similar_images_g,similar_image_vectors_g
+    similar_images_g,similar_image_vectors_g = p3task5.task5b(query_image, 20)
+    print(similar_images_g,similar_image_vectors_g)
+    return similar_images_g,similar_image_vectors_g
+
+def get_training_set(feedback_imgs,feedback_vals):
+    x_train=[]
+    y_train=[]
+    global similar_images_g,similar_image_vectors_g
+    global feedback_imgs_g, feedback_vals_g
+    # update overall feedback
+    for img,val in zip(feedback_imgs,feedback_vals):
+    # if user changes their feedback later
+        if img in feedback_imgs_g:
+            index = feedback_imgs_g.index(img)
+            feedback_vals_g[index] = int(val)
+    # new feedback
+        else:
+            feedback_imgs_g.append(img)
+            feedback_vals_g.append(int(val))
+    
+    for i in range(len(similar_images_g)):
+        for j in range(len(feedback_imgs)):
+            if similar_images_g[i] == feedback_imgs[j]:
+                x_train.append(similar_image_vectors_g[similar_images_g[i]])
+                y_train.append(feedback_vals_g[j])
+    return x_train,y_train
+
+"""
+Method to incorporate relevance feedback
+"""
+def rerank_results(feedback,similar_images,similar_image_vectors):
+    global feedback_imgs_g,feedback_vals_g,similar_images_g,similar_image_vectors_g
+    similar_images_g = similar_images
+    similar_image_vectors_g = similar_image_vectors
+    # feedback = {'Hand_0000071.jpg': '1', 'Hand_0000073.jpg': '1'}
+    # feedback = {'Hand_0000071.jpg': '-1', 'Hand_0000073.jpg': '-1','Hand_0000074.jpg': '1', 'Hand_0000070.jpg': '-1', 'Hand_0000075.jpg': '-1', 'Hand_0000078.jpg': '1', 'Hand_0009068.jpg': '1', 'Hand_0007705.jpg': '1', 'Hand_0000076.jpg': '-1', 'Hand_0007228.jpg': '-1', 'Hand_0009162.jpg': '-1', 'Hand_0009163.jpg': '-1'}
+    # Add SVM based relevance feedback function
+    clf = SupportVectorMachine(gaussian_kernel, C=500)
+    feedback_imgs = list(feedback.keys())
+    feedback_vals = list(feedback.values())
+    x_train,y_train = get_training_set(feedback_imgs,feedback_vals)
+    clf.fit(np.array(x_train), np.array(y_train))
+    x_test = similar_image_vectors_g
+    image_dist_SVM = clf.project(list(x_test.values()))
+    image_dist_SVM_index = [i[0] for i in sorted(enumerate(image_dist_SVM), key=lambda x:x[1], reverse=True)]
+    rel_similar_images = [list(similar_image_vectors_g.keys())[index] for index in image_dist_SVM_index] 
+    # list(similar_image_vectors_g.keys())[image_dist_SVM_index]
+    return rel_similar_images
+
 def main():
     query_image = "Hand_0000072.jpg"
-    # results = search_LSH(query_image)
-    results = ['Hand_0000072.jpg', 'Hand_0000073.jpg', 'Hand_0000074.jpg', 'Hand_0000112.jpg']
-    global similar_images_g,similar_image_vectors_g
-    # similar_images_g = list(i['imageId'] for i in results)
-    # similar_image_vectors_g = list(i['reducedDimensions'] for i in results)
-    
-    similar_images_g = results
-
-    # rerank_results(None)
-    relevancefeedback.relevance_fdbk(os.path.abspath("Hands"),"SVM",query_image,similar_images_g)
+    similar_images,img_vectors = get_LSH_results(query_image)
+    # while True:
+    #     rerank_results(None)
+    relevancefeedback.relevance_fdbk("SVM",query_image,similar_images,img_vectors)
     pass
 
 if __name__ == "__main__":
--- utils/relevancefeedback.py
+++ utils/relevancefeedback.py
@@ -1,29 +1,29 @@
-import os
-
 from flask import Flask, request, render_template, send_from_directory, redirect
 
 import task6_svm as task6_svm
 import task6d_probability
+import os
 
 app = Flask(__name__, template_folder=(os.path.abspath('templates')))
 classifier_g = None
 query_image_g = None
 similar_images_g = None
+similar_image_vectors_g = None
 port_g = 4558
 
 
-def relevance_fdbk(data_dir, classifier, query_image, similar_images):
+def relevance_fdbk(classifier, query_image, similar_images,similar_image_vectors):
     """
     initial call to relevance feedback.
     Load app server
     Set global vars
     """
-    print(data_dir)
-    global classifier_g, query_image_g, similar_images_g
+    global classifier_g, query_image_g, similar_images_g,similar_image_vectors_g
 
     classifier_g = classifier
     query_image_g = query_image
     similar_images_g = similar_images
+    similar_image_vectors_g = similar_image_vectors
     global port_g
     print("\nClick here: http://localhost:{0}/similar_images\n".format(port_g))
     app.run(port=port_g, debug=True)
@@ -45,8 +45,7 @@ def display_similar_images():
     if request.method == 'POST':
         data = request.form
         global similar_images_g, query_image_g
-        similar_images_g = incorporate_feedback(data.to_dict())
-        # pdb.set_trace()
+        similar_images_g,similar_image_vectors_g = incorporate_feedback(data.to_dict())
         return redirect("http://localhost:{0}/similar_images".format(port_g), code=303)
     elif request.method == 'GET':
         return render_template("relevancefeedback.html", image_names=[query_image_g, similar_images_g])
@@ -56,9 +55,9 @@ def incorporate_feedback(data):
     """
     Method to call the chosen classifier based feedback system
     """
-    global classifier_g
+    global classifier_g,similar_images_g,similar_image_vectors_g
     if classifier_g == "SVM":
-        rel_similar_images = task6_svm.rerank_results(data)
+        rel_similar_images = task6_svm.rerank_results(data,similar_images_g,similar_image_vectors_g)
     elif classifier_g == "PROBABILITY":
         rel_similar_images = task6d_probability.rerank_results(data)
-    return rel_similar_images
+    return rel_similar_images, similar_image_vectors_g
