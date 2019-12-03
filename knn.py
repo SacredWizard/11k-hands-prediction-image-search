50b890099a75603a7ce937aa98b137e8ca1d6dcb
karishma-joseph <kjoseph7@asu.edu>
2019-12-03 00:15:13 -0700

    Added KNN

---
+++ knn.py
@@ -0,0 +1,30 @@
+import numpy as np
+import scipy
+
+
+class KNN(object):
+    def __init__(self, k):
+        self.k_value = k
+        self.X_train = []
+        self.y_train = []
+
+    def fit(self, X_train, y_train):
+        self.X_train = X_train
+        self.y_train = y_train
+
+    # Euclidean distance
+    def euclidean(self, x, y):
+        dist = scipy.spatial.distance.cdist(x, y, 'euclidean')
+        return dist
+
+    # Make a classification prediction with neighbors
+    def predict(self, test):
+        predictions = []
+        dist_matrix = self.euclidean(test, self.X_train)
+        for row in dist_matrix:
+            neighbor_indices = np.array(row).argsort()[:self.k_value]
+            output_values = [self.y_train[i] for i in neighbor_indices]
+            prediction = max(set(output_values), key=output_values.count)
+            predictions.append(prediction)
+        return predictions
+
