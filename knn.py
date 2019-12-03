1279ff384903ff4aca88db8e63bcb079290ec86a
karishma-joseph <kjoseph7@asu.edu>
2019-12-03 00:15:27 -0700

    Modified KNN

--- knn.py
+++ knn.py
@@ -1,6 +1,11 @@
 import numpy as np
 import scipy
 
+# Euclidean distance
+def euclidean(x, y):
+    dist = scipy.spatial.distance.cdist(x, y, 'euclidean')
+    return dist
+
 
 class KNN(object):
     def __init__(self, k):
@@ -12,15 +17,10 @@ class KNN(object):
         self.X_train = X_train
         self.y_train = y_train
 
-    # Euclidean distance
-    def euclidean(self, x, y):
-        dist = scipy.spatial.distance.cdist(x, y, 'euclidean')
-        return dist
-
     # Make a classification prediction with neighbors
     def predict(self, test):
         predictions = []
-        dist_matrix = self.euclidean(test, self.X_train)
+        dist_matrix = euclidean(test, self.X_train)
         for row in dist_matrix:
             neighbor_indices = np.array(row).argsort()[:self.k_value]
             output_values = [self.y_train[i] for i in neighbor_indices]
