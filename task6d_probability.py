49d61261753dfd77e3d5777091954058c5b3908a
Rakesh R <rakesh2693@gmail.com>
2019-12-03 18:40:29 -0700

    reformatting and integrating

--- task6d_probability.py
+++ task6d_probability.py
@@ -1,13 +1,12 @@
 import math
 import operator
-import os
 
 import numpy as np
 
 import task5
 import utils.relevancefeedback as relevancefeedback
-from utils.model import Model
 from utils.inputhelper import get_input_image
+from utils.model import Model
 
 feedback_metadata_obj = {}
 user_relevant_images = []
@@ -17,7 +16,7 @@ user_irrelevant_images = []
 def rerank_results(data):
     model = Model()
     for k in data.keys():
-        if data[k] == 1:
+        if data[k] == str(1):
             user_relevant_images.append(k)
             if k in user_irrelevant_images:
                 user_irrelevant_images.remove(k)
@@ -30,7 +29,11 @@ def rerank_results(data):
     bin_matrix = model.load_model('bin_matrix')
     imageid_index = model.load_model('imageid_index')
     feature_index = model.load_model('feature_index')
-    get_relevance(feat_vectors.keys(), bin_matrix, imageid_index, feature_index, feat_vectors)
+    print('Relevant')
+    print(user_relevant_images)
+    print('Irrelevant')
+    print(user_irrelevant_images)
+    return get_relevance(feat_vectors.keys(), bin_matrix, imageid_index, feature_index, feat_vectors)
 
 
 def get_relevance(imageids, bin_matrix, imageid_index, feature_index, feat_vectors):
@@ -41,6 +44,7 @@ def get_relevance(imageids, bin_matrix, imageid_index, feature_index, feat_vecto
     image_relevance = []
     for image, feature in sorted(similar_image.items(), key=operator.itemgetter(1), reverse=True):
         image_relevance.append(image)
+    print(similar_image)
     return image_relevance
 
 
@@ -73,11 +77,8 @@ def get_image_similarity(imageid, imageid_index, feature_index, feat_vectors, bi
     feature_vec = bin_matrix[imageid_index[imageid]]
 
     for feat in feature_index:
-        if feat in feedback_metadata_obj.keys():
-            (pi, ui) = feedback_metadata_obj[feat]
-        else:
-            (pi, ui) = get_feedback_metadata(feature_index[feat], imageid_index, bin_matrix)
-            feedback_metadata_obj[feat] = (pi, ui)
+        (pi, ui) = get_feedback_metadata(feature_index[feat], imageid_index, bin_matrix)
+        feedback_metadata_obj[feat] = (pi, ui)
         t = feature_vec[feature_index[feat]] * (math.log((pi * (1 - ui)) / (ui * (1 - pi))))
         score += t
 
@@ -112,9 +113,9 @@ def get_probability_revelance_feedback(query_id="Hand_0000003.jpg"):
     model.save_model(bin_matrix, 'bin_matrix')
     model.save_model(imageid_index, 'imageid_index')
     model.save_model(feature_index, 'feature_index')
-    relevancefeedback.relevance_fdbk(os.path.abspath("Hands"), "PROBABILITY", query_id,
+    relevancefeedback.relevance_fdbk("PROBABILITY", query_id,
                                      get_relevance(feat_vectors.keys(), bin_matrix, imageid_index, feature_index,
-                                                   feat_vectors))
+                                                   feat_vectors), feat_vectors)
 
 
 def main():
@@ -124,4 +125,3 @@ def main():
 
 if __name__ == '__main__':
     main()
-
